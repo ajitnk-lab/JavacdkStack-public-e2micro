18f92c6725e36455b61594de5b0927c3fd914a6f
ajitnklab <ajitnk.lab@gmail.com>
2024-07-31 12:33:27 +0530

    first commit

--- src/main/java/com/myorg/JavacdkStack.java
+++ src/main/java/com/myorg/JavacdkStack.java
@@ -3,8 +3,13 @@ package com.myorg;
 import software.constructs.Construct;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
-// import software.amazon.awscdk.Duration;
-// import software.amazon.awscdk.services.sqs.Queue;
+
+import java.util.List;
+
+import software.amazon.awscdk.Duration;
+import software.amazon.awscdk.services.sqs.Queue;
+
+import software.amazon.awscdk.services.ec2.*;
 
 public class JavacdkStack extends Stack {
     public JavacdkStack(final Construct scope, final String id) {
@@ -16,9 +21,28 @@ public class JavacdkStack extends Stack {
 
         // The code that defines your stack goes here
 
-        // example resource
+        // // example resource
         // final Queue queue = Queue.Builder.create(this, "JavacdkQueue")
-        //         .visibilityTimeout(Duration.seconds(300))
-        //         .build();
+        //          .visibilityTimeout(Duration.seconds(300))
+        //          .build();
+
+          // Create a VPC with one public subnet
+        Vpc vpc = Vpc.Builder.create(this, "MyVpc")
+                .maxAzs(1)
+                .subnetConfiguration(List.of(
+                    SubnetConfiguration.builder()
+                            .cidrMask(24)
+                            .name("Public")
+                            .subnetType(SubnetType.PUBLIC)
+                            .build()
+                ))
+                .build();
+
+        // Create an EC2 instance in the public subnet
+        Instance instance = Instance.Builder.create(this, "MyInstance")
+                .vpc(vpc)
+                .instanceType(InstanceType.of(InstanceClass.T2, InstanceSize.MICRO))
+                .machineImage(MachineImage.latestAmazonLinux2023())
+                .build();
     }
 }
